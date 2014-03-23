0970021c89ab97ee70657818248d7f322f3cb632
MedicineYeh <medicinehy@gmail.com>
2014-03-24 00:51:10 +0800

    fixed kill bug

--- kill.sh
+++ kill.sh
@@ -1,3 +1,6 @@
-ps aux | grep -ie 'python bg.py' | awk '{print $2}' | xargs kill -9
+#!/bin/bash                                                                                                                    
+id=$(ps aux | grep -ie 'python bg.py' | grep -v 'sudo' | awk '{print $2}' | xargs)
+sudo kill -9 $id
+
 ps aux | grep -ie 'node app' | awk '{print $2}' | xargs kill -9
 
