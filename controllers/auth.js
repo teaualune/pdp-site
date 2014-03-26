615f9c2d167c92f6493993a7e9eaefd65fe07062
teaualune <teaualune@gmail.com>
2014-03-26 11:49:55 +0800

    open cross grading permission for uploaded hw

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
-var emailValidation = require('../config/email-validation'),
+var path = require('path'),
+    _ = require('underscore'),
+    emailValidation = require('../config/email-validation'),
     settings = require('../settings.json'),
+    CrossGrading = require('../model/cross-grading'),
     auth;
 
 auth = {
@@ -40,16 +43,37 @@ auth = {
         }
     },
     uploadDir: function (req, res, next) {
+        var url;
         if (req.isAuthenticated() && req.user) {
             if (req.user.admin) {
                 // admin
                 next();
             } else {
                 // student
-                if (req.originalUrl.indexOf(emailValidation.getStudentID(req.user.email)) > 0 || req.originalUrl.indexOf('team' + req.user.team) > 0) {
+                url = req.originalUrl;
+                if (url.indexOf(emailValidation.getStudentID(req.user.email)) > 0 || url.indexOf('team' + req.user.team) > 0) {
                     next();
                 } else {
-                    res.send(401);
+                    CrossGrading.findByAuthor(req.user._id).populate('submission').exec(function (err, cgs) {
+                        var found = false;
+                        if (err) {
+                            res.send(500);
+                        } else if (cgs) {
+                            _.each(cgs, function (cg) {
+                                var fileName = _.last(cg.submission.filePath.split(path.sep));
+                                if (url.indexOf(fileName) > 0) {
+                                    found = true;
+                                }
+                            });
+                            if (found) {
+                                next();
+                            } else {
+                                res.send(401);
+                            }
+                        } else {
+                            res.send(404);
+                        }
+                    });
                 }
             }
         } else {
